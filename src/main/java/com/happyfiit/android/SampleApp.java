9595e56388ab9660a29d3314a939051cc8a6b0a8
odybour <odybour@gmail.com>
2015-07-16 22:52:51 +0300

    initial commit

---
+++ src/main/java/com/happyfiit/android/SampleApp.java
@@ -0,0 +1,67 @@
+package com.happyfiit.android;
+
+import android.app.Application;
+
+import com.happyfiit.sdk.ApplicationConfig;
+import com.happyfiit.sdk.FirstRunHandler;
+import com.happyfiit.sdk.Happyfiit;
+import com.happyfiit.sdk.Opportunity;
+import com.happyfiit.sdk.Workout;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class SampleApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put(ApplicationConfig.DEVELOPER_KEY, "test_user_key");
+        initParams.put(ApplicationConfig.DEVELOPER_SECRET, "jDQpJea7klf7fT6y7jk821pHu2m2461U");
+        initParams.put(ApplicationConfig.USER_EMAIL, "odybour@gmail.com");
+
+        //Happyfiit.initialize(getApplicationContext(), initParams, firstRunHandler());
+        Happyfiit.initialize(getApplicationContext(), initParams);
+    }
+
+
+    private FirstRunHandler firstRunHandler() {
+        return new FirstRunHandler() {
+            @Override
+            public Opportunity getWorkoutHistory() {
+
+                //create user's workout history
+
+                Workout workout1 = new Workout.Builder()
+                        .type("running")
+                        .addWorkoutStatistic("distance", "meter", "100")
+                        .addWorkoutStatistic("duration", "sec", "10")
+                        .addWorkoutStatistic("avg. speed", "km/h", new Double(100 / 10).toString())
+                        .build();
+
+                Workout workout2 = new Workout.Builder()
+                        .type("running")
+                        .addWorkoutStatistic("distance", "meter", "100")
+                        .addWorkoutStatistic("duration", "sec", "9")
+                        .addWorkoutStatistic("avg. speed", "km/h", new Double(100 / 9).toString())
+                        .build();
+
+                List<Workout> workouts = new ArrayList<>(Arrays.asList(workout1, workout2));
+
+                Opportunity op = new Opportunity.Builder()
+                        .history(true)
+                        .addWorkoutList(workouts)
+                        .addParameter("pb", 10)
+                        .build();
+
+                return op;
+            }
+        };
+    }
+}
---
+++ src/main/java/com/happyfiit/android/WorkoutSimulatorActivity.java
@@ -0,0 +1,93 @@
+package com.happyfiit.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.happyfiit.sdk.HappyfiitCallbackHandler;
+import com.happyfiit.sdk.Happyfiit;
+import com.happyfiit.sdk.Reward;
+import com.happyfiit.sdk.exceptions.HappyfiitInitializationException;
+import com.happyfiit.sdk.Opportunity;
+import com.happyfiit.sdk.Workout;
+
+public class WorkoutSimulatorActivity extends Activity implements View.OnClickListener {
+
+    private static final String TAG = WorkoutSimulatorActivity.class.getSimpleName();
+
+    private Happyfiit happyfiit;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.workout_simulator);
+
+        // Capture our button from layout
+        Button button = (Button) findViewById(R.id.simulatorButtonView);
+
+        // Register the onClick listener with the implementation above
+        button.setOnClickListener(this);
+
+        try {
+            happyfiit = Happyfiit.getInstance();
+        } catch (HappyfiitInitializationException e) {
+            e.printStackTrace();
+        }
+
+        happyfiit.start(new HappyfiitCallbackHandler(this) {
+            @Override
+            public void onFailure(Exception ex) {
+
+            }
+        });
+
+    }
+
+    // Implement the OnClickListener callback
+    @Override
+    public void onClick(View view) {
+        String seconds = ((EditText) findViewById(R.id.durationValueView)).getText().toString();
+        String meters = ((EditText) findViewById(R.id.distanceValueView)).getText().toString();
+
+        Log.d(TAG, "Seconds:" + seconds);
+        Log.d(TAG, "Distance:" + meters);
+
+        Opportunity op = new Opportunity.Builder()
+                .addWorkout(new Workout.Builder()
+                        .type("running")
+                        .addWorkoutStatistic("distance", "meter", meters)
+                        .addWorkoutStatistic("duration", "sec", seconds)
+                        .addWorkoutStatistic("avg. speed", "km/h", new Double(Integer.parseInt(meters) / Integer.parseInt(seconds)).toString())
+                        .build())
+                .addParameter("pb", 10)
+                .build();
+
+        happyfiit.pushOpportunity(op, new HappyfiitCallbackHandler(this) {
+
+            /**
+             * Optionally the user can override the onAchievement() method of the HappyfiitCallbackHandler class to implement custom handling logic
+             * like showing a native application notification (e.g. popup, present/gift icon etc.) prior to showing the Happyfiit Reward popup.
+             * This will allow the application developer to maintain control over the user experience by customizing the look and feel of the notification.
+             *
+             * Important note: When you override this method you MUST make sure that at some point you call the showReward() method of the Happyfiit API
+             * so that the user can redeem the reward.
+             * @param reward
+             */
+            @Override
+            public void onAchievement(Reward reward) {
+                //1. Create a native to the application notification (optional)
+
+                //2. Show Happyfiit reward popup so that user can redeem it (required)
+                happyfiit.showReward(activityContext, reward);
+            }
+
+            @Override
+            public void onFailure(Exception ex) {
+                //handle exception
+            }
+        });
+    }
+}
